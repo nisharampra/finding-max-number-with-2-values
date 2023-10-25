07cc3ce8d4f6b5b1c47028a0704e88b68dbcdc42
Nisha Ramprasath <115043583+nisharampra@users.noreply.github.com>
2023-10-25 17:44:44 +0800

    Add files via upload

---
+++ finding max number with 2 values/finding which number is maximum/main.cpp
@@ -0,0 +1,24 @@
+//
+//  main.cpp
+//  finding which number is maximum
+//
+//  Created by Nisha Ramprasath on 25/10/23.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int x,y;
+cout<<"enter 2 no";
+cin>>x>>y;
+if(x>y){
+cout<<"max is "<<x;
+}
+else{
+    cout<<"max is "<<y;
+}
+    return 0;
+}
